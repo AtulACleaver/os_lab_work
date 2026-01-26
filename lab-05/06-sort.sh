2bf33f219eb55024078a8fce5da9ec49a327dc4e
atulacleaver <atulcleaver@gmail.com>
2026-01-26 23:21:44 +0530

    Update 06-sort.sh

--- lab-05/06-sort.sh
+++ lab-05/06-sort.sh
@@ -6,3 +6,21 @@ echo "Enter the numbers separated by space:"
 read -a numbers
 
 n=${#numbers[@]}
+
+for (( i = 0; i < n; i++ ))
+do
+    for (( j = i + 1; j < n; j++ ))
+    do
+        if (( numbers[i] < numbers[j] ))
+        then
+            temp=${numbers[i]}
+            numbers[i]=${numbers[j]}
+            numbers[j]=$temp
+        fi
+    done
+done
+echo "Numbers in descending order:"
+for num in "${numbers[@]}"
+do
+    echo "$num"
+done
